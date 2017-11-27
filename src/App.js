25271f19f3a8a4851f363f33e4f75591cc8821c2
Toby Ladislas & Nicole Gasperini <pair+majesty86.nicole7@devbootcamp.com>
2017-11-27 16:51:35 -0600

    Get state to pass values for each counter release 3

--- src/App.js
+++ src/App.js
@@ -3,16 +3,41 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+  constructor() {
+    super()
+
+    this.state = {
+      totalCounterValues: 0,
+      counts: () =>
+
+      // counts: {
+      //   first: 0,
+      //   second: 0,
+      //   third: 0
+      // }
+      // we want counts to be a function
+    }
+  }
+
+  increment = (countBy, key) => {
+    this.setState({
+      counts: {
+        first: 5
+      }
+    });
+    console.log(this.state)
+    // console.log(countBy, countState)
+  }
+
   render() {
     return (
       <div className="page-center-frame">
-        <Counter countBy={1}/>
-        <Counter countBy={2} />
-        <Counter countBy={3} />
+        <Counter countBy={1} countState={this.state.countOfFirst} increment={this.increment} />
+        <Counter countBy={2} increment={this.increment} />
+        <Counter countBy={3} increment={this.increment} />
       </div>
     );
   }
 }
 
-//This line makes the App class available for inclusion in other files like index.js
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -2,34 +2,28 @@ import React from 'react';
 import './Counter.css';
 
 class Counter extends React.Component {
-  constructor() {
-    super();
 
-    this.state = {
-      count: 0
-    };
-  }
+  // increment = (countBy) => {
+  //   this.setState({ count: this.state.count + this.props.countBy })
+  // }
 
-
-  increment = (countBy) => {
-    this.setState({ count: this.state.count + this.props.countBy })
-  }
-
-  decrement = (countBy) => {
-    this.setState({ count: this.state.count - this.props.countBy })
-  }
+  // We're going to need this
+  // decrement = (countBy) => {
+  //   this.setState({ count: this.state.count - this.props.countBy })
+  // }
 
   render() {
+    console.log(this.props)
     return (
       <div className="counter">
-        <button onClick={this.increment}>+</button>
-        <div className="count">{this.state.count}</div>
-        <button onClick={this.decrement}>-</button>
+        <button onClick={() => this.props.increment(this.props.countBy, this.props.countState)}>+</button>
       </div>
       )
   }
 }
 
+        // <div className="count">{this.state.count}</div>
+        // <button onClick={this.decrement}>-</button>
 
 
 export default Counter
