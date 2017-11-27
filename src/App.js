b3f3b90baf0b433b98dfbc5c043895244aa9378f
Graham Troyer-Joy <graham@troyer-joy.com>
2017-11-27 17:56:44 -0600

    breakout -- some things might not be 100% great

--- src/App.js
+++ src/App.js
@@ -3,12 +3,58 @@ import Counter from './Counter';
 import './App.css';
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0,
+      counterValues: [
+        {
+          value: 0,
+          howMuchToIncrement: 1
+        },
+        {
+          value: 0,
+          howMuchToIncrement: 2
+        }
+      ]
+    };
+
+    this.increment = this.increment.bind(this)
+    this.decrement = this.decrement.bind(this)
+  }
+
+  increment(index, howMuchToIncrement) {
+    this.setState((prevState, props) => {
+      prevState.counterValues[index].value += howMuchToIncrement;
+      return prevState;
+    });
+  }
+
+  decrement(index, howMuchToIncrement) {
+    this.setState((prevState, props) => {
+      prevState.counterValues[index].value -= howMuchToIncrement;
+      return prevState;
+    });
+  }
+
   render() {
+    const foos = this.state.counterValues.map((value, index, wholeArray) => {
+        console.log(value);
+
+      return (
+        <Counter indexToModify={ index } count={ value.value } increment={ this.increment } decrement={ this.decrement } howMuchToIncrement={ value.howMuchToIncrement }  />
+      )
+    })
+
+
+    console.log(foos)
     return (
       <div className="page-center-frame">
-        <Counter />
-        <Counter />
-        <Counter />
+        {
+          foos
+        }
       </div>
     );
   }
